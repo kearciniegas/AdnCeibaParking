bba2d1fcd708146ba136b642b808a6ad9d4c9ea0
Kevin Enrique Arciniegas Chacón - Ceiba Software <kevin.arciniegas@ceiba.com.co>
2019-06-05 15:07:27 -0500

    ajuste controlador

--- src/main/java/com/ceiba/induccion/comun/infrastructure/controller/VehiculoController.java
+++ src/main/java/com/ceiba/induccion/comun/infrastructure/controller/VehiculoController.java
@@ -1,10 +1,26 @@
 package com.ceiba.induccion.comun.infrastructure.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/vehiculos")
-public class VehiculoController {
+import com.ceiba.induccion.comun.application.dto.VehiculoDto;
+import com.ceiba.induccion.comun.domain.entity.Registro;
+import com.ceiba.induccion.comun.domain.entity.TipoVehiculo;
+import com.ceiba.induccion.comun.domain.entity.Vehiculo;
+import com.ceiba.induccion.microservicios.command.infraestructura.RegistrarEntradaVehiculos;
 
+@RestController
+@RequestMapping("vehiculos")
+public class VehiculoController {
+	@Autowired
+	private RegistrarEntradaVehiculos registrarEntradaCommad;
+
+	@PostMapping
+	public Registro registrarIngreso(@RequestBody VehiculoDto vehiculoDto) {
+		return registrarEntradaCommad.execute(new Vehiculo(vehiculoDto.getId(), vehiculoDto.getPlaca(),
+				TipoVehiculo.valueOf(vehiculoDto.getTipo()), vehiculoDto.getCilindraje(), null));
+	}
 }
