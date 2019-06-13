b419d50fca36eac48e09d7248b30c097a4765075
kevinarciniegas <kevin.arciniegas@ceiba.com.co>
2019-06-13 13:09:46 -0500

    ajustes

--- src/test/java/com/ceiba/induccion/Domain/unitarias/ServiceVigilantTest.java
+++ src/test/java/com/ceiba/induccion/Domain/unitarias/ServiceVigilantTest.java
@@ -1,6 +1,6 @@
 package com.ceiba.induccion.domain.unitarias;
 
-import static org.mockito.Mockito.when;
+/*import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,44 +22,38 @@ import com.ceiba.induccion.infrastructure.repository.RegistryRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+*/
 public class ServiceVigilantTest {
+
 	/*
-	private static final String PLACA_VEHICULO_SIN_RESTRICCION = "NNL677";
-	private static final Integer CILINDRAJE_MOTO = 550;
-	private static final int MOTOS_EN_PARQUEADERO = 3;
-
-	@Mock
-	private VigilantImpl calendarioVigilante;
-
-	@Mock
-	private RulesParkingImpl reglasEstacionamiento;
-
-	@Mock
-	private RegistryRepository registryRepository;
-
-	@InjectMocks
-	private VigilantActivitiesImpl operacionesDelVigilante;
-
-	@Before
-	public void setUp() {
-		MockitoAnnotations.initMocks(ServiceVigilantTest.class);
-	}
-
-	 @Test
-	public void registrarMotoConCupoTest() {
-		// arrange
-		Registry registry = RegistryBuilder.defaultValues().conPlaca(PLACA_VEHICULO_SIN_RESTRICCION).conCilindraje(CILINDRAJE_MOTO).conVehicleType(VehicleType.MOTO).build();
-
-		when(registryRepository.contarVehiculosEstacionados(VehicleType.MOTO)).thenReturn(MOTOS_EN_PARQUEADERO);
-
-		when(reglasEstacionamiento.validarSiHayEspacio(VehicleType.MOTO, MOTOS_EN_PARQUEADERO))
-				.thenReturn(Boolean.TRUE);
-		// act
-		Registry registroAlmacenado = operacionesDelVigilante.registrarEntrada(registry);
-
-		// assert
-		Assert.assertEquals(PLACA_VEHICULO_SIN_RESTRICCION, registroAlmacenado.getPlaca());
-		Assert.assertEquals(VehicleType.MOTO, registroAlmacenado.getVehicleType());
-	}*/
-
+	 * private static final String PLACA_VEHICULO_SIN_RESTRICCION = "NNL677";
+	 * private static final Integer CILINDRAJE_MOTO = 550; private static final int
+	 * MOTOS_EN_PARQUEADERO = 3;
+	 * 
+	 * @Mock private VigilantImpl calendarioVigilante;
+	 * 
+	 * @Mock private RulesParkingImpl reglasEstacionamiento;
+	 * 
+	 * @Mock private RegistryRepository registryRepository;
+	 * 
+	 * @InjectMocks private VigilantActivitiesImpl operacionesDelVigilante;
+	 * 
+	 * @Before public void setUp() {
+	 * MockitoAnnotations.initMocks(ServiceVigilantTest.class); }
+	 * 
+	 * @Test public void registrarMotoConCupoTest() { // arrange Registry registry =
+	 * RegistryBuilder.defaultValues().conPlaca(PLACA_VEHICULO_SIN_RESTRICCION)
+	 * .conCilindraje(CILINDRAJE_MOTO).conVehicleType(VehicleType.MOTO).build();
+	 * 
+	 * when(registryRepository.contarVehiculosEstacionados(VehicleType.MOTO)).
+	 * thenReturn(MOTOS_EN_PARQUEADERO);
+	 * 
+	 * when(reglasEstacionamiento.validarSiHayEspacio(VehicleType.MOTO,
+	 * MOTOS_EN_PARQUEADERO)) .thenReturn(Boolean.TRUE); // act Registry
+	 * registroAlmacenado = operacionesDelVigilante.registrarEntrada(registry);
+	 * 
+	 * // assert Assert.assertEquals(PLACA_VEHICULO_SIN_RESTRICCION,
+	 * registroAlmacenado.getPlaca()); Assert.assertEquals(VehicleType.MOTO,
+	 * registroAlmacenado.getVehicleType()); }
+	 */
 }
